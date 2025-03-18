12eb6b4f484f7eea3c25bd666c0194c1bbacb053
KacperLewa <kacperlewandowski491@gmail.com>
2025-03-18 16:00:46 +0100

    Added Deck Class

--- blackjack/src/main/java/com/mycompany/blackjack/Deck.java
+++ blackjack/src/main/java/com/mycompany/blackjack/Deck.java
@@ -8,7 +8,7 @@ package com.mycompany.blackjack;
  *
  * @author egzamin
  */
-public class Blackjack {
+public class Deck {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
